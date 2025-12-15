a21323e6f64b256ef48a94b225eff092a14fc077
farline99 <farline99@proton.me>
2025-12-15 12:52:39 +0300

    Создание базового интерфейса пользователя

--- LightBoard/Form1.Designer.cs
+++ LightBoard/Form1.Designer.cs
@@ -28,18 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnCalculate = new Button();
+            lblResult = new Label();
+            groupBox1 = new GroupBox();
             SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(12, 32);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(94, 29);
+            btnCalculate.TabIndex = 0;
+            btnCalculate.Text = "Рассчитать";
+            btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(12, 9);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(211, 20);
+            lblResult.TabIndex = 1;
+            lblResult.Text = "Нажмите кнопку для расчета";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(12, 67);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(370, 238);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Визуализация";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(394, 317);
+            Controls.Add(groupBox1);
+            Controls.Add(lblResult);
+            Controls.Add(btnCalculate);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCalculate;
+        private Label lblResult;
+        private GroupBox groupBox1;
     }
 }
