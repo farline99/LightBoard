d74819f80626e244f35492f566b973f6aa335e01
farline99 <farline99@proton.me>
2025-12-15 13:07:22 +0300

    Добавлен Enum состояний и метод отрисовки цвета лампочки

--- LightBoard/Form1.cs
+++ LightBoard/Form1.cs
@@ -1,10 +1,35 @@
 namespace LightBoard
 {
+    public enum BulbState
+    {
+        Off = 0,      // Выключено
+        On = 1,       // Включено
+        Blink = 2     // Мигает
+    }
+
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void DrawBulb(PictureBox pb, BulbState state)
+        {
+            switch (state)
+            {
+                case BulbState.Off:
+                    pb.BackColor = Color.Gray;
+                    break;
+                case BulbState.On:
+                    pb.BackColor = Color.Yellow;
+                    break;
+                case BulbState.Blink:
+                    pb.BackColor = Color.Orange;
+                    break;
+            }
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
