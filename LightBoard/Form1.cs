fb98793d711cad4812ef9c6a653993535559e31f
farline99 <farline99@proton.me>
2025-12-15 13:23:14 +0300

    Финальный рефакторинг кода и комментарии

--- LightBoard/Form1.cs
+++ LightBoard/Form1.cs
@@ -25,13 +25,13 @@ namespace LightBoard
             switch (state)
             {
                 case BulbState.Off:
-                    pb.BackColor = Color.Gray;
+                    pb.BackColor = Color.Gray; // Серый - выкл
                     break;
                 case BulbState.On:
-                    pb.BackColor = Color.Yellow;
+                    pb.BackColor = Color.Yellow; // Желтый - вкл
                     break;
                 case BulbState.Blink:
-                    pb.BackColor = Color.Orange;
+                    pb.BackColor = Color.Orange; // Оранжевый - условно "мигает"
                     break;
             }
         }
@@ -43,6 +43,7 @@ namespace LightBoard
             int bulbs = 0;
             int currentCombinations = 1;
 
+            // Цикл пока комбинаций меньше чем нужно сигналов
             while (currentCombinations < signalsNeeded)
             {
                 bulbs++;
@@ -52,26 +53,29 @@ namespace LightBoard
             lblResult.Text = $"Для {signalsNeeded} сигналов нужно {bulbs} лампочки (комбинаций: {currentCombinations})";
 
             btnNextSignal.Enabled = true;
-            currentSignalIndex = 0;
+            currentSignalIndex = 0; // Сброс при новом расчете
         }
 
         private void btnNextSignal_Click(object sender, EventArgs e)
         {
+            // Ограничиваем 18 сигналами (0..17)
             if (currentSignalIndex >= 18) currentSignalIndex = 0;
 
             int temp = currentSignalIndex;
 
-            int state1 = temp % 3;
+            // Получаем состояния для 3 лампочек (деление с остатком на 3)
+            int digitForRight = temp % 3;
             temp /= 3;
 
-            int state2 = temp % 3;
+            int digitForMiddle = temp % 3;
             temp /= 3;
 
-            int state3 = temp % 3;
+            int digitForLeft = temp % 3;
 
-            DrawBulb(bulb1, (BulbState)state1);
-            DrawBulb(bulb2, (BulbState)state2);
-            DrawBulb(bulb3, (BulbState)state3);
+            // Отрисовываем
+            DrawBulb(bulb3, (BulbState)digitForRight);
+            DrawBulb(bulb2, (BulbState)digitForMiddle);
+            DrawBulb(bulb1, (BulbState)digitForLeft);
 
             lblResult.Text = $"Сигнал № {currentSignalIndex}";
 
